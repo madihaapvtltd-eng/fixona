dfbaee575ebccf2aa69836c280491e8e9f65fad8
Mohamed Maushaz <maushaz@madihaa.com.mv>
2026-03-29 12:23:53 +0500

    Fix JSX structure

--- web/src/pages/settings/AdminSettingsPage.tsx
+++ web/src/pages/settings/AdminSettingsPage.tsx
@@ -455,6 +455,7 @@ export function AdminSettingsPage() {
           ))}
         </div>
       )}
+      </div>
 
       {/* Departments Section */}
       <div className="bg-white rounded-2xl shadow-lg p-6 border border-gray-100">
